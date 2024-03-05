6e9bf3dd8fa6796f7524d84144e9f0afe4b2fe57
mohanad <mohanadhassan246@gmail.com>
2024-03-05 17:13:02 +0200

    check-comments

--- src/pages/Products/ProductList.jsx
+++ src/pages/Products/ProductList.jsx
@@ -133,6 +133,7 @@ export default function ProductList() {
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
+        {/* Check */}
           <Box sx={style}>
             {/* <AddProduct closeEvent={handleClose} /> */}
           </Box>
